cc5b4fba8f881498e83a026088f46e018056817c
Sarah-Marion <devsarahmarion@gmail.com>
2025-09-23 00:05:37 +0300

    Academic Research Frameworks

---
+++ examples/academic/kenyan_research_protocols.py
@@ -0,0 +1,29 @@
+"""
+Academic Research Module - Sovereign OSINT Toolkit
+Ethical research frameworks for Kenyan contexts
+By Sarah Marion
+"""
+
+class KenyanResearchFramework:
+    """Academic OSINT that meets Kenyan IRB and ethical standards"""
+    
+    def create_research_protocol(self, topic: str, methodology: str):
+        """Generate research protocols compliant with Kenyan standards"""
+        
+        protocol_template = {
+            "local_approval_required": [
+                "National Commission for Science, Technology and Innovation",
+                "University Ethical Review Board", 
+                "County Research Permits"
+            ],
+            "community_engagement": [
+                "Stakeholder consultation meetings",
+                "Community advisory board",
+                "Benefit-sharing agreement"
+            ],
+            "data_sovereignty": [
+                "Data stored on Kenyan servers",
+                "Local researcher access priority",
+                "Publication rights for Kenyan institutions"
+            ]
+        }
