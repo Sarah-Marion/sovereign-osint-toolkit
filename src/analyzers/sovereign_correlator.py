12c57c63a56d531229dc3635a73850673ac5e962
Sarah-Marion <devsarahmarion@gmail.com>
2025-09-23 00:10:13 +0300

    Data Correlation Engine with Bias Awareness

---
+++ src/analyzers/sovereign_correlator.py
@@ -0,0 +1,29 @@
+"""
+Sovereign Data Correlation Engine
+Context-aware analysis that detects colonial bias patterns
+By Sarah Marion
+"""
+
+class SovereignCorrelator:
+    """Correlation engine that understands Kenyan context"""
+    
+    def detect_colonial_bias(self, dataset: dict, analysis_method: str):
+        """Identify and flag colonial bias in data analysis"""
+        
+        bias_patterns = {
+            "western_assumptions": [
+                "individualistic_frameworks", 
+                "universal_applicability",
+                "decontextualized_metrics"
+            ],
+            "extractive_methodologies": [
+                "data_mining_without_consent",
+                "export_analysis_abroad", 
+                "local_knowledge_extraction"
+            ],
+            "context_ignorance": [
+                "ignoring_historical_trauma",
+                "disregarding_local_expertise",
+                "imposing_foreign_categories"
+            ]
+        }
