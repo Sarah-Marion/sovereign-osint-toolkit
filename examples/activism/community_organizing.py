df50e64fb67cfa6f88eeece9abcac4f04ee14c14
Sarah-Marion <devsarahmarion@gmail.com>
2025-09-22 20:03:16 +0300

    Use Case Implementation: Activism

---
+++ examples/activism/community_organizing.py
@@ -0,0 +1,25 @@
+"""
+Community organizing and advocacy OSINT tools
+For Kenyan civil society and activists
+"""
+
+class CommunityOrganizingTools:
+    """Tools for ethical community organizing intelligence"""
+    
+    def map_power_structures(self, constituency: str):
+        """Map local power structures for advocacy targeting"""
+        
+        power_actors = {
+            "formal": ["MP", "MCA", "County Executive", "Administration"],
+            "informal": ["religious_leaders", "business_associations", "community_elders"],
+            "influencers": ["youth_leaders", "women_leaders", "activists"]
+        }
+    
+    def monitor_public_service_delivery(self):
+        """Track government service delivery for advocacy"""
+        services_to_monitor = [
+            "health_facility_stockouts",
+            "school_infrastructure", 
+            "water_point_functionality",
+            "road_maintenance_schedules"
+        ]
